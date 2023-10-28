5bde2d366f5cf5edcb62508c6c2b8ee27e20b719
Felipe Matos <felipematosapps@gmail.com>
2023-10-28 14:40:39 -0300

    terceiro commit adicionado botoes, imagens e texto na activity_main e criado um nova classe ResultadoActivity

--- app/src/main/java/com/example/calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_imc/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.calculadora_imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener{
+
+           val intent = Intent(this,ResultadoActivity::class.java)
+
+            startActivity(intent)
+
+        }
+
     }
 }
